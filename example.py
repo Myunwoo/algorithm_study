7c4e2a3d3956826ce72b5e0fbc5cd67634a687fc
Myunwoo <63415602+Myunwoo@users.noreply.github.com>
2021-06-20 18:32:10 +0900

    2021/06/20

--- example.py
+++ example.py
@@ -1,26 +1,2 @@
-N = int(input())
+from collections import deque
 
-moves = list(input().split())
-
-pos =[0,0]
-
-for move in moves:
-    if move == 'R':
-        if (pos[0] + 1) >= N:
-            continue
-        pos[0] += 1
-    elif move == 'L':
-        if (pos[0] - 1) < 0:
-            continue
-        pos[0] -= 1
-    elif move == 'U':
-        if (pos[1] - 1) < 0:
-            continue
-        pos[1] -= 1
-    
-    elif move == 'D':
-        if (pos[1] + 1) >= N:
-            continue
-        pos[1] += 1
-
-print(str(pos[1]+1) + " " + str(pos[0]+1))

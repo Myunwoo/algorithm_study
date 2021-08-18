32d84ed4ce071a26c3e97cf74b6191158ac29958
Myunwoo <fddsgt123@gmail.com>
2021-08-18 18:39:53 +0900

    Create baek2212.py

---
+++ sort/baek2212.py
@@ -0,0 +1,21 @@
+#https://www.acmicpc.net/problem/2212
+import sys
+N=int(input())
+K=int(input())
+sensors=list(map(int, input().split()))
+
+
+if K >= N:
+    print(0)
+    sys.exit()
+
+sensors.sort()
+dist=[]
+for i in range(len(sensors)-1):
+    dist.append(sensors[i+1]-sensors[i])
+dist.sort(reverse=True)
+
+for _ in range(K-1):
+    dist.pop(0)
+
+print(sum(dist))

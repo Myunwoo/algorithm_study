37ed5bf60a2450cc47b1c638486a5f4273f1c41a
Myunwoo <fddsgt123@gmail.com>
2021-08-29 11:16:12 +0900

    1

---
+++ sort/baek11728.py
@@ -0,0 +1,9 @@
+#https://www.acmicpc.net/problem/11728
+N,M=map(int, input().split())
+A=list(map(int, input().split()))
+B=list(map(int, input().split()))
+
+C=A+B
+C.sort()
+for c in C:
+    print(c,end=' ')

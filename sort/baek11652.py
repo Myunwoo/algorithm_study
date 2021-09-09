77ac46156646e80e4dfe3e32a8d4e13179e5b046
Myunwoo <fddsgt123@gmail.com>
2021-09-09 23:09:15 +0900

    Update baek11652.py

--- sort/baek11652.py
+++ sort/baek11652.py
@@ -11,3 +11,18 @@ cards=Counter(cards).most_common()
 cards.sort(key=lambda x:[-x[1],x[0]])
 
 print(cards[0][0])
+
+#####딕셔너리를 활용한 풀이
+n = int(input())
+dic = {}
+
+for case in range(n):
+    tmp = int(input())
+    if tmp in dic:
+        dic[tmp] += 1
+    else:
+        dic[tmp] = 1
+
+dic = sorted(dic.items(), key=lambda x: (-x[1], x[0]))
+print(dic)
+print(dic[0][0])

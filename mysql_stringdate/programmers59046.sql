224e70056b7de47c7c157ad204bece03384e4100
Myunwoo <fddsgt123@gmail.com>
2021-11-20 03:34:02 +0900

    string date

---
+++ mysql_stringdate/programmers59046.sql
@@ -0,0 +1,13 @@
+-- https://programmers.co.kr/learn/courses/30/lessons/59046
+-- 첫 풀이
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME = 'Lucy' OR
+NAME='Ella' OR
+NAME='Pickle' OR
+NAME='Rogan' OR
+NAME='Sabrina' OR
+NAME='Mitty'
+ORDER BY ANIMAL_ID;
+
+

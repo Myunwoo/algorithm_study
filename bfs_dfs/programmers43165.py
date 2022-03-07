6acba24062b841f72914b1a7cdead74b42ccbafb
Myunwoo <fddsgt123@gmail.com>
2022-03-08 06:34:08 +0900

    Update programmers43165.py

--- bfs_dfs/programmers43165.py
+++ bfs_dfs/programmers43165.py
@@ -1,17 +1,19 @@
 def solution(numbers, target):
-    answer = 0
-    
-    def dfs(result, i):
-        nonlocal numbers
-        #배열을 모두 탐색했다면, target과 일치하는지만 확인
-        if i == len(numbers):
-            if result == target:
-                nonlocal answer
-                answer += 1
+    count=0
+    def dfs(pre, index):
+        #이전까지의 총합에 현재 인덱스의 수를 더하고, 빼기
+        l=pre+numbers[index]
+        r=pre-numbers[index]
+        #끝에 다다른 경우
+        if index==len(numbers)-1:
+            nonlocal count
+            if l==target:
+                count+=1
+            if r==target:
+                count+=1
             return
-        else:
-            #result에 인덱스의 값을 더해주고 넘기는 것으로 dfs
-            dfs(i+1, result+numbers[i])
-            dfs(i+1, result-numbers[i])
+        dfs(l,index+1)
+        dfs(r,index+1) 
     dfs(0,0)
-    return answer
+        
+    return count

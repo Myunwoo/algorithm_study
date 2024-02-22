abf512c215951942a5bbfd921a4494419a6750f5
Munwoo <fddsgt123@gmail.com>
2024-02-22 09:42:15 +0900

    1

---
+++ solved.ac_3/baek10026.py
@@ -0,0 +1,5 @@
+from collections import deque
+
+N = int(input())
+graph = [input().split() for _ in range(N)]
+
--- solved.ac_3/baek7662.py
+++ solved.ac_3/baek7662.py
@@ -5,26 +5,37 @@ input = sys.stdin.readline
 answer = []
 T = int(input())
 for _ in range(T):
-    heap = []
+    minHeap = []
+    maxHeap = []
+    visited = [False for _ in range(1000001)]
     k = int(input())
-    for _ in range(k):
-        o, v = input().split()
-        if o == 'I':
-            heapq.heappush(heap, int(v))
-        else:
-            if not heap:
-                continue
-            if v == '1':
-                heap.pop()
-            else:
-                heapq.heappop(heap)
-    if not heap:
-        answer.append('EMPTY')
-    else:
-        answer.append([max(heap), min(heap)])
+    for index in range(k):
+        operation, num = input().split()
+        if operation == 'I':
+            heapq.heappush(minHeap, (int(num), index))
+            heapq.heappush(maxHeap, (int(num)*-1, index))
+            visited[index] = True
+        elif operation == 'D':
+            if num == '1' and not maxHeap:
+                v, i = heapq.heappop(maxHeap)
+                visited[i] = False
+            elif num == '-1' and not minHeap:
+                v, i = heapq.heappop(minHeap)
+                visited[i] = False
 
-for a in answer:
-    if a != 'EMPTY':
-        print(a[0], a[1])
-    else:
-        print(a)
+    small = float('inf')
+    big = float('inf')*-1
+
+    while minHeap:
+        v, i = heapq.heappop(minHeap)
+        if visited[i]:
+            small = min(small, v)
+            break
+    
+    while maxHeap:
+        v, i = heapq.heappop(maxHeap)
+        if visited[i]:
+            big = max(big*-1, v)
+            break
+
+    print(big, small)

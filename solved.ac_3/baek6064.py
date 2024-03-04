7f7cad17e2af8b174ed6273f4fea9fbe2a47bbe8
Munwoo <fddsgt123@gmail.com>
2024-03-05 00:47:17 +0900

    Update baek6064.py

--- solved.ac_3/baek6064.py
+++ solved.ac_3/baek6064.py
@@ -1,32 +1,18 @@
-T = int(input())
-answer = []
+import sys
 
-for _ in range(T):
-  M, N, x, y = list(map(int, input().split()))
-  curX, curY = 1, 1
-  if x==1 and y==1:
-    answer.append(1)
-    continue
-  
-  count = 1
-  found = False
-  while True:
-    curX += 1
-    curY += 1
-    count += 1
-    if curX > M:
-      curX = 1
-    if curY > N:
-      curY = 1
 
-    if curX == x and curY == y:
-      found = True
-      answer.append(count)
-      break
-    if curX == M and curY == N:
-      break
-  if not found:
-    answer.append(-1)
+def calculate(m, n, x, y):
+    k = x #k를 x로 초기화
+    while k <= m * n: #k의 범위는 m*n을 넘을 수 없기에
+        if (k - x) % m == 0 and (k - y) % n == 0: #2개의 조건을 만족하는 k값을 찾는다.
+            return k
+        k += m #k-x가 m의 배수이기 때문에 k는 x로 초기화해주었기 때문에 m만 더해준다.
+    return -1
 
-for a in answer:
-  print(a)
+
+t = int(input())
+
+for _ in range(t):
+    m, n, x, y = map(int, sys.stdin.readline().split())
+
+    print(calculate(m, n, x, y))

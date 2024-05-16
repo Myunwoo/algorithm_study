f95146f953ebcf49bcb638746dbc7f804e42bda3
임현우_FO-FE개발 DEV_소프트웍스 <fddsgt1234@softworks.co.kr>
2024-05-16 10:50:09 +0900

    Update baek1012.py

--- solved.ac_3/baek1012.py
+++ solved.ac_3/baek1012.py
@@ -2,38 +2,35 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-def bfs(N, M, yard, n, m):
-    dq = deque()
-    dq.append([n, m])
-    yard[n][m] = 0
+def bfs(N, M, n, m, graph):
+    dq=deque()
     dn = [1, -1, 0, 0]
     dm = [0, 0, 1, -1]
+    dq.append([n, m])
+    graph[n][m] = 0
+
     while dq:
         curN, curM = dq.popleft()
         for i in range(4):
             newN = curN + dn[i]
             newM = curM + dm[i]
-            if 0 <= newN < N and 0 <= newM < M and yard[newN][newM] == 1:
+            if 0<=newN<N and 0<=newM<M and graph[newN][newM]==1:
+                graph[newN][newM] = 0
                 dq.append([newN, newM])
-                yard[newN][newM] = 0
-
 
 T = int(input())
-answer = []
 for _ in range(T):
     count = 0
-    M, N, K = list(map(int, input().split()))
-    yard = [[0 for _ in range(M)] for _ in range(N)]
+    M, N, K = map(int, input().strip().split())
+    graph = [[0 for _ in range(M)] for _ in range(N)]
     for _ in range(K):
-        x, y = list(map(int, input().split()))
-        yard[y][x] = 1
+        x, y = map(int, input().strip().split())
+        graph[y][x] = 1
     
-    for n in range(N):
-        for m in range(M):
-            if yard[n][m] == 1:
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j] == 1:
+                bfs(N, M, i, j, graph)
                 count += 1
-                bfs(N, M, yard, n, m)
-    answer.append(count)
-
-for a in answer:
-    print(a)
+    
+    print(count)

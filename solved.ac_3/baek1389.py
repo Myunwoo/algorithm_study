e19071d49e0a7553406b59d973b8d7368b02e837
임현우_FO-FE개발 DEV_소프트웍스 <fddsgt1234@softworks.co.kr>
2024-05-16 12:48:03 +0900

    Update baek1389.py

--- solved.ac_3/baek1389.py
+++ solved.ac_3/baek1389.py
@@ -1,19 +1,36 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-N, M = list(map(int, input().split()))
+N, M = map(int, input().strip().split())
 graph = [[float('inf') for _ in range(N)] for _ in range(N)]
 for _ in range(M):
-  a, b = list(map(int,input().split()))
-  graph[a][b] = 1
-  graph[b][a] = 1
+    A, B = map(int, input().strip().split())
+    graph[A-1][B-1] = 1
+    graph[B-1][A-1] = 1
 
-# i 점에서 출발하여
+# 각 사람을 거쳐가는 최단거리 갱신
 for i in range(N):
-  # j 점을 거칠 때
-  for j in range(N):
-    if i == j:
-      continue
-    for idx in range(N):
-      graph[i][j] = min(graph[i][j], graph[i][idx])
-    
+    for j in range(N):
+        for k in range(N):
+            if j == k:
+                graph[j][k]=0
+            else:
+              # j에서 k로 가는 길 = min(j에서 i를 거쳐 k로 가는 것, j에서 k로 가는 것)
+              cost = min(graph[j][i]+graph[i][k], graph[j][k])
+              graph[j][k] = cost
+              graph[k][j] = cost
+
+minCost = float('inf')
+who = -1
+for i in range(N):
+    s = 0
+    for j in range(len(graph[i])):
+        if i == j:
+            continue
+        s += graph[i][j]
+    if s < minCost:
+        minCost = s
+        who = i+1
+
+print(who)

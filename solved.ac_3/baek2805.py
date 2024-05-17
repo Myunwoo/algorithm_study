c359c8354403cdd9ae5b81af6e3690547fe58b45
임현우_FO-FE개발 DEV_소프트웍스 <fddsgt1234@softworks.co.kr>
2024-05-17 13:31:47 +0900

    Update baek2805.py

--- solved.ac_3/baek2805.py
+++ solved.ac_3/baek2805.py
@@ -1,8 +1,27 @@
 import sys
-input = sys.stdin.readline
+N,M=map(int, sys.stdin.readline().split())
+trees=list(map(int, sys.stdin.readline().strip().split()))
 
-N, M = list(map(int, input().rstrip().split()))
-heights = list(map(int, input().rstrip().split()))
-heights.sort()
+l, r = 0, max(trees)
+goal = r
 
-start, end = 1, 2000000000
+# 이분탐색의 시간 복잡도는 O(logN)이다.
+# N의 최대가 1000000, M의 최대가 2000000000 이므로,
+# 1,000,000 * log2,000,000,000 = 9백만 정도.
+while l <= r:
+    mid = (r+l) // 2
+    s = 0
+    for tree in trees:
+        if tree-mid > 0:
+            s+=tree-mid
+
+    if s==M:
+        goal = mid
+        break
+    elif s<M:
+        r = mid-1
+        goal = r
+    elif s>M:
+        l = mid+1
+
+print(goal)

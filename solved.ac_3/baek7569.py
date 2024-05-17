79574d4c5ad6e874059c6a8675d22fc2e2351002
임현우_FO-FE개발 DEV_소프트웍스 <fddsgt1234@softworks.co.kr>
2024-05-17 14:50:49 +0900

    Update baek7569.py

--- solved.ac_3/baek7569.py
+++ solved.ac_3/baek7569.py
@@ -4,30 +4,36 @@ input = sys.stdin.readline
 
 M, N, H = list(map(int, input().strip().split()))
 graph = [[list(map(int, input().strip().split())) for _ in range(N)] for _ in range(H)]
-cost = [[float('inf') for _ in range(M)] for _ in range(N) for _ in range(H)]
+dq = deque()
+for i in range(H):
+  for j in range(N):
+    for k in range(M):
+      if graph[i][j][k] == 1:
+        dq.append((i, j, k))
 
 dh = [0, 0, 1, -1, 0, 0]
 dn = [0, 0, 0, 0, 1, -1]
 dm = [1, -1, 0, 0, 0, 0]
 
-def bfs(graph, cost, h, n, m):
-  print(h, n, m)
-  cost[h][n][m] = 0
-  dq = deque()
-  dq.append((h, n, m))
-  while dq:
-    curH, curN, curM = dq.popleft()
-    for i in range(6):
-      newH = curH + dh[i]
-      newN = curN + dn[i]
-      newM = curM + dm[i]
-      if 0<=newH<H and 0<=newN<N and 0<=newM<M and (cost[newH][newN][newM] > cost[curH][curN][curM]+1):
-        cost[newH][newN][newM] = cost[curH][curN][curM]+1
-        dq.append((newH, newN, newM))
+while dq:
+  curH, curN, curM = dq.popleft()
+  for i in range(6):
+    newH = curH + dh[i]
+    newN = curN + dn[i]
+    newM = curM + dm[i]
+    if 0<=newH<H and 0<=newN<N and 0<=newM<M and (graph[newH][newN][newM] == 0):
+      graph[newH][newN][newM] = graph[curH][curN][curM]+1
+      dq.append((newH, newN, newM))
 
 
+m = -1
 for i in range(H):
   for j in range(N):
     for k in range(M):
-      if graph[i][j][k] == 1:
-        bfs(graph, cost, i, j, k)
+      if graph[i][j][k] == 0:
+        print(-1)
+        exit()
+      else:
+        m = max(m, graph[i][j][k])
+
+print(m-1)

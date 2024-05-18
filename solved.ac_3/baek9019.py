b10cd8476d62c53f6ca3186bf08e6e8c3c900e7e
Munwoo <fddsgt123@gmail.com>
2024-05-18 17:09:51 +0900

    Update baek9019.py

--- solved.ac_3/baek9019.py
+++ solved.ac_3/baek9019.py
@@ -1,38 +1,38 @@
-T = int(input())
+import sys
 from collections import deque
-
-def bfs(A, B):
-    visited = [False for _ in range(10001)]
-    word = ''
-    dq = deque()
-    dq.append((A, word))
-    visited[A] = True
-    while dq:
-        a, w = dq.popleft()
-        if a == B:
-            return w
-        # D
-        d = (a * 2) % 10000
-        if not visited[d]:
-            visited[d] = True
-            dq.append((d, w+'D'))
-        # S
-        s= (a - 1) % 10000
-        if a - 1 > 0 and not visited[s]:
-            visited[s] = True
-            dq.append((s, w+'S'))
-        # L
-        l = a // 1000 + (a % 1000)*10
-        if not visited[l]:
-            visited[l] = True
-            dq.append((l, w+'L'))
-        # R
-        r = a // 10 + (a % 10) * 1000
-        if not visited[r]:
-            visited[r] = True
-            dq.append((r, w+'R'))
-
+T = int(input())
 
 for _ in range(T):
-    A, B = map(int, input().split())
-    print(bfs(A, B))
+    A, B = map(int,sys.stdin.readline().rstrip().split())
+
+    visited = [False for i in range(10001)]
+    deq = deque()
+    deq.append([A,''])
+    visited[A] = True
+
+    while deq:
+        num, command = deq.popleft()
+
+        if num == B:
+            print(command)
+            break
+
+        d = num * 2 % 10000
+        if not visited[d]:
+            visited[d] = True
+            deq.append([d, command + 'D'])
+
+        s = (num - 1) % 10000
+        if not visited[s]:
+            visited[s] = True
+            deq.append([s, command + 'S'])
+
+        l = num // 1000 + (num % 1000)*10
+        if not visited[l]:
+            visited[l] = True
+            deq.append([l, command + 'L'])
+
+        r = num // 10 + (num % 10) * 1000
+        if not visited[r]:
+            visited[r] = True
+            deq.append([r, command + 'R'])

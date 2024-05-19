c872013bfbb32386ecc087e2d0312358286ef39e
Munwoo <fddsgt123@gmail.com>
2024-05-20 00:01:10 +0900

    Update baek7662.py

--- solved.ac_3/baek7662.py
+++ solved.ac_3/baek7662.py
@@ -2,24 +2,6 @@ import sys
 import heapq
 input = sys.stdin.readline
 
-T = int(input())
-for _ in range(T):
-    k = int(input())
-    hq = []
-    for _ in range(k):
-        operand, val = input().strip().split()
-        val = int(val)
-        if operand == 'I':
-            heapq.heappush(hq, (val, -val))
-        elif operand == 'D' and hq:
-            if val == 1:
-                hq.pop()
-            elif val == -1:
-                heapq.heappop(hq)
-    
-    if len(hq) > 1:
-        print(hq[len(hq)-1][0], hq[0][0])
-    elif len(hq) == 1:
-        print(hq[0][0], hq[0][0])
-    elif len(hq) == 0:
-        print('EMPTY')
+# 이 문제는 최소 힙과 최대 힙 두 가지를 동시에 관리하여 풀이하는 문제입니다.
+# 중복되는 값에 대한 동기화는 dic 자료형을 사용하여 해결 가능합니다.
+# 나중에 도전해 보도록 합시다

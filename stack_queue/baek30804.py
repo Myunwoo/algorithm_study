76d8e396b9b33defbf09fc4cef0f79c51170f5fa
임현우_FO-FE개발 DEV_소프트웍스 <fddsgt123@gmail.com>
2024-06-17 17:38:40 +0900

    Update baek30804.py

--- stack_queue/baek30804.py
+++ stack_queue/baek30804.py
@@ -1,2 +1,24 @@
+from collections import Counter
+
 N = int(input())
-fruits = int(input().split())
+nums = list(map(int, input().split()))
+
+left, right = 0, 1
+result = 1
+countArr = [0 for _ in range(10)]
+countArr[nums[0]] += 1
+
+while right < N:
+    count = 0
+    for i in range(10):
+        if countArr[i] > 0:
+            count += 1
+    if count > 2:
+        countArr[nums[left]] -= 1
+        left += 1
+    else:
+        result = max(result, right-left)
+        countArr[nums[right]] += 1
+        right += 1
+
+print(result)

009bcb2dbb209ce13cec58c0f7ca8a40c0b0f00c
Munwoo <fddsgt123@gmail.com>
2024-07-01 14:42:34 +0900

    Update baek2609.py

--- math/baek2609.py
+++ math/baek2609.py
@@ -1,1 +1,12 @@
 nums = list(map(int, input().split()))
+
+def GCD(a, b):
+  while b:
+    a, b = b, a%b
+  return a
+
+def LCM(a, b):
+  return (a*b) // GCD(a, b)
+
+
+print(GCD(nums[0], nums[1]), LCM(nums[0], nums[1]))

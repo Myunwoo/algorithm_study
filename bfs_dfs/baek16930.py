70dbd80e39177898eda4f1e1100cbb6020a3b26b
임현우_FO-FE개발 DEV_소프트웍스 <fddsgt123@gmail.com>
2024-07-15 10:10:05 +0900

    Update baek16930.py

--- bfs_dfs/baek16930.py
+++ bfs_dfs/baek16930.py
@@ -1,3 +1,41 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, M, K = map(int, input().strip().split())
+graph = [list(input().strip()) for _ in range(N)]
+costs = [[sys.maxsize for _ in range(M)] for _ in range(N)]
+startN, startM, endN, endM = map(int, input().strip().split())
+costs[startN-1][startM-1] = 0
+
+def bfs():
+    global costs
+    dn = [1, -1, 0, 0]
+    dm = [0, 0, 1, -1]
+    dq = deque()
+    dq.append([startN-1, startM-1])
+    while dq:
+        curN, curM = dq.popleft()
+        if curN == endN-1 and curM == endM-1:
+            break
+        curCost = costs[curN][curM]
+        for i in range(4):
+            for k in range(1, K+1):
+                newN = curN + dn[i]*k
+                newM = curM + dm[i]*k
+                if 0>newN or newN>=N or 0>newM or newM>=M or graph[newN][newM] == '#':
+                    break
+                if costs[newN][newM]>curCost+1:
+                    costs[newN][newM] = curCost+1
+                    dq.append([newN, newM])
+
+bfs()
+
+print(-1 if costs[endN-1][endM-1] == sys.maxsize else costs[endN-1][endM-1])
+
+
+
+### 정답코드
 from collections import deque
 import sys
 input = sys.stdin.readline

f39686cf6b00c66e71f030d6585f0d2714773ae6
Munwoo <fddsgt123@gmail.com>
2024-07-29 00:14:20 +0900

    Update programmers85944.py

--- sort/programmers85944.py
+++ sort/programmers85944.py
@@ -4,6 +4,7 @@ def solution(score):
     sorted_score = sorted(score, reverse=True)
     
     for s in score:
+        # index로 최초일치를 가져오므로, 공동 등수 처리 가능
         answer.append(sorted_score.index(s)+1)
     
     return answer

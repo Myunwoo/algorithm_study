d19943c248831030784131455fdd9e6369018897
Munwoo <fddsgt123@gmail.com>
2024-08-05 23:13:10 +0900

    Create programmers85951.py

---
+++ greedy/programmers85951.py
@@ -0,0 +1,12 @@
+def solution(routes):
+    answer = 0
+    cur = -30001
+    routes.sort(key=lambda x:[x[1], x[0]])
+    
+    for i in range(len(routes)):
+        start, end = routes[i]
+        if start > cur:
+            cur = end
+            answer += 1
+            
+    return answer

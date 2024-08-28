9c970b1edbc11d9b80d729236b27a5056d2a6c04
임현우_FO-FE개발 DEV_소프트웍스 <fddsgt123@gmail.com>
2024-08-28 10:21:22 +0900

    Update programmers85966.py

--- implementation/programmers85966.py
+++ implementation/programmers85966.py
@@ -9,10 +9,17 @@ def solution(cap, n, deliveries, pickups):
     for i in range(n):
         have_to_deli += deliveries[i]
         have_to_pick += pickups[i]
+        print('i', i)
+        print('have_to_deli', have_to_deli)
+        print('have_to_pick', have_to_pick)
 
         while have_to_deli > 0 or have_to_pick > 0:
             have_to_deli -= cap
             have_to_pick -= cap
             answer += (n - i) * 2
+        print('answer', answer)
+        print()
 
     return answer
+
+print(solution(4, 5, 	[1, 0, 3, 1, 2], [0, 3, 0, 4, 0]))

77abcaf07b177cd290e2087693a16e3417fd3472
Munwoo <fddsgt123@gmail.com>
2025-04-11 23:49:31 +0900

    java

---
+++ java_training/programmers12928.java
@@ -0,0 +1,15 @@
+class Solution {
+  public int solution(int n) {
+    int answer = 0;
+    int idx = 1;
+
+    while (idx <= n) {
+      if (n % idx == 0) {
+        answer += idx;
+      }
+      idx++;
+    }
+
+    return answer;
+  }
+}

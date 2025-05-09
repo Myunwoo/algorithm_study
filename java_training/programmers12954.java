2badeef6b70b949631fecdc4ea626159d181ef0d
Munwoo <fddsgt123@gmail.com>
2025-05-10 00:21:56 +0900

    java

---
+++ java_training/programmers12954.java
@@ -0,0 +1,12 @@
+public class programmers12954 {
+  public long[] solution(int x, int n) {
+    long[] answer = new long[n];
+    long start = x;
+    for (int i = 0; i < n; i++) {
+      answer[i] = start;
+      start += x;
+    }
+
+    return answer;
+  }
+}

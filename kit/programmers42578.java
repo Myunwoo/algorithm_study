445865b356e6f0feb5210cdcfd08bbe388d97c49
Munwoo <fddsgt123@gmail.com>
2025-05-15 20:31:20 +0900

    Create programmers42578.java

---
+++ kit/programmers42578.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        
+        for (String[] clothe: clothes) {
+            String val = clothe[0];
+            String typ = clothe[1];
+            
+            hashMap.put(typ, hashMap.getOrDefault(typ, 1) + 1);
+        }
+        
+        for (String key: hashMap.keySet()) {
+            answer *= hashMap.get(key);
+        }
+        
+        return answer - 1;
+    }
+}

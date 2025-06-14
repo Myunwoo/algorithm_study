b003355fde951f1bc48f274e13ee42fc64804c37
Myunwoo <fddsgt123@gmail.com>
2025-06-14 10:28:17 +0900

    Create programmers12912.java

---
+++ java_training/programmers12912.java
@@ -0,0 +1,8 @@
+import java.util.stream.LongStream;
+
+class Solution {
+    public long solution(int a, int b) {
+        if (a == b) return a;
+        return LongStream.rangeClosed(Math.min(a, b), Math.max(a, b)).sum();
+    }
+}

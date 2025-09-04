8ca79f1af8a75ef82720c0ba4264db8764278ff3
Munwoo <fddsgt123@gmail.com>
2025-09-04 22:09:01 +0900

    Update leetcode344.py

--- nol/leetcode344.py
+++ nol/leetcode344.py
@@ -1,7 +1,6 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        for i in range(len(s)//2):
+        idx = len(s) // 2
+
+        for i in range(idx):
             s[i], s[len(s)-i-1] = s[len(s)-i-1], s[i]

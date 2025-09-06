256bbd7881075812571c06e7def41094cf41e2b9
Myunwoo <fddsgt123@gmail.com>
2025-09-06 23:09:58 +0900

    Update leetcode412.py

--- nol/leetcode412.py
+++ nol/leetcode412.py
@@ -1,13 +1,12 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        answer = []
-        for i in range(1,n+1):
-            if i % 3 == 0 and i % 5 == 0:
-                answer.append('FizzBuzz')
-            elif i % 3 == 0:
-                answer.append('Fizz')
-            elif i % 5 == 0:
-                answer.append('Buzz')
-            else:
-                answer.append(str(i))
-        return answer
+        base = [
+            "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", 
+            "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"
+        ]
+        result = []
+        for i in range(1, n+1):
+            result.append(base[(i-1) % 15].replace(
+                str((i-1) % 15 + 1), str(i)
+            ))
+        return result

38829f9d1810ae0eff6859a32b1aaed7fc2d462a
Myunwoo <fddsgt123@gmail.com>
2025-10-01 23:55:24 +0900

    Update leetcode326.py

--- leetcode/leetcode326.py
+++ leetcode/leetcode326.py
@@ -1,30 +1,14 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        if n <= 0:
+        if n < 1:
             return False
         
-        idx = 0
-        while True:
-            cur = 3**idx
-            if cur == n:
+        def recursion(num):
+            if num == 1:
                 return True
-            elif cur > n:
-                return False
+            elif num % 3 == 0:
+                return recursion(num // 3)
             else:
-                idx += 1
-
-
-class Solution:
-    def isPowerOfThree(self, n: int) -> bool:
-        if n == 1:
-            return True
-        elif n < 3:
-            return False
-        
-        def recursion(n):
-            if n == 1:
-                return True
-            if n % 3 != 0:
                 return False
-            return recursion(n//3)
+
         return recursion(n)

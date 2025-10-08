345b6151c2fdc559becb959a228dd7376f61038d
Munwoo <fddsgt123@gmail.com>
2025-10-09 04:21:12 +0900

    1009

--- SQL/SELECT/programmers164673.sql
+++ SQL/SELECT/programmers164673.sql
@@ -1,9 +1,9 @@
 SELECT
-    ugb.TITLE,
-    ugb.BOARD_ID,
-    ugr.REPLY_ID,
-    ugr.WRITER_ID,
-    ugr.CONTENTS,
+    ugb.TITLE as TITLE,
+    ugb.BOARD_ID as BOARD_ID,
+    ugr.REPLY_ID as REPLY_ID,
+    ugr.WRITER_ID as WRITER_ID,
+    ugr.CONTENTS as CONTENTS,
     DATE_FORMAT(ugr.CREATED_DATE, '%Y-%m-%d') as CREATED_DATE
 FROM
     USED_GOODS_BOARD as ugb
@@ -14,8 +14,6 @@ ON
 WHERE
     DATE_FORMAT(ugb.CREATED_DATE, '%Y-%m') = '2022-10'
 ORDER BY
-    ugr.CREATED_DATE, ugb.TITLE;
-
-# DATE_FORMAT(ugb.CREATED_DATE, '%Y-%m') = '2022-10'
-# SUBSTR(ugb.CREATED_DATE,1,7)='2022-10'
+    ugr.CREATED_DATE ASC,
+    ugb.TITLE ASC
     
--- leetcode/leetcode3.py
+++ leetcode/leetcode3.py
@@ -1,31 +1,36 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # 문자가 마지막으로 등장한 위치를 저장할 딕셔너리
-        char_index = {}
+        if len(s) < 2:
+            return len(s)
+        
+        hash = {}
+        hash[s[0]] = 0
+        curIdx = 0
+        answer = 0
 
-        # 현재 부분 문자열의 시작 위치 (왼쪽 포인터)
-        left = 0
+        for i in range(1, len(s)):
+            if s[i] in hash:
+                curIdx = max(curIdx, hash[s[i]] + 1)
+            answer = max(answer, i-curIdx+1)
+            hash[s[i]] = i
+        
+        return answer
 
-        # 가장 긴 부분 문자열의 길이를 저장할 변수
-        max_len = 0
 
-        # 문자열의 각 문자를 오른쪽 포인터(right)로 순회
-        for right in range(len(s)):
-            current_char = s[right]
 
-            # 만약 현재 문자가 이미 등장했고,
-            # 그 인덱스가 현재 윈도우(left ~ right) 안에 있다면,
-            # 중복이므로 왼쪽 포인터를 그 다음 위치로 이동시켜야 한다.
-            if current_char in char_index and char_index[current_char] >= left:
-                # 중복된 문자를 피하기 위해 left를 해당 위치 다음으로 이동
-                left = char_index[current_char] + 1
 
-            # 현재 문자의 인덱스를 딕셔너리에 기록 (항상 갱신)
-            char_index[current_char] = right
 
-            # 현재 윈도우의 길이를 계산하고, 최댓값 갱신
-            window_length = right - left + 1
-            max_len = max(max_len, window_length)
 
-        # 결과 반환
-        return max_len
+def lengthOfLongestSubstring(s):
+    hash = {}
+    answer = 0
+    curIdx = 0
+ 
+    for i in range(len(s)):
+        if s[i] in hash:
+            curIdx = max(curIdx, hash[s[i]] + 1)
+ 
+        hash[s[i]] = i
+        answer = max(answer, i - curIdx + 1)
+ 
+    return answer

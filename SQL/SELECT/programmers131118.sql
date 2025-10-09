6b3fe2faca684758b3c178ea445e4112b8a8bdbd
Munwoo <fddsgt123@gmail.com>
2025-10-09 21:40:11 +0900

    1009

---
+++ SQL/SELECT/programmers131118.sql
@@ -0,0 +1,26 @@
+SELECT
+    ri.REST_ID AS REST_ID,
+    ri.REST_NAME AS REST_NAME,
+    ri.FOOD_TYPE AS FOOD_TYPE,
+    ri.FAVORITES AS FAVORITES,
+    ri.ADDRESS AS ADDRESS,
+    rr.SCORE AS SCORE
+FROM
+    REST_INFO AS ri
+INNER JOIN
+    (
+        SELECT
+            REST_ID,
+            ROUND(AVG(REVIEW_SCORE),2) AS SCORE
+        FROM
+            REST_REVIEW
+        GROUP BY
+            REST_ID
+    ) AS rr
+ON
+    ri.REST_ID = rr.REST_ID
+WHERE
+    ri.ADDRESS LIKE '서울%'
+ORDER BY
+    SCORE DESC,
+    FAVORITES DESC
--- mysql_select/programmers59405.sql
+++ mysql_select/programmers59405.sql
@@ -1,5 +1,7 @@
--- https://programmers.co.kr/learn/courses/30/lessons/59405
-SELECT NAME
-FROM ANIMAL_INS
-ORDER BY DATETIME
+SELECT
+    NAME
+FROM
+    ANIMAL_INS
+ORDER BY
+    DATETIME
 LIMIT 1

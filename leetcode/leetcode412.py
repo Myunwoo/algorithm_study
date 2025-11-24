94a6bd5a025bfbd5747af8031f573b96d6070e22
Myunwoo <fddsgt123@gmail.com>
2025-11-24 23:34:45 +0900

    Update leetcode412.py

--- leetcode/leetcode412.py
+++ leetcode/leetcode412.py
@@ -1,23 +1,20 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        answer = []
+        answer = ['0' for _ in range(n)]
         for i in range(1, n+1):
+            value = ''
             if i % 3 == 0 and i % 5 == 0:
-                answer.append('FizzBuzz')
+                value = 'FizzBuzz'
             elif i % 3 == 0:
-                answer.append('Fizz')
+                value = 'Fizz'
             elif i % 5 == 0:
-                answer.append('Buzz')
+                value = 'Buzz'
             else:
-                answer.append(str(i))
+                value = str(i)
+            answer[i-1] = value
         return answer
-        base = [
-            "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", 
-            "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"
-        ]
-        result = []
-        for i in range(1, n+1):
-            result.append(base[(i-1) % 15].replace(
-                str((i-1) % 15 + 1), str(i)
-            ))
-        return result
+    
+# 이 답변의 시간 복잡도는 O(N)이라고 하기 쉬움. 그러나,
+# str(i)가 잡아먹는 시간 까지를 따지자면 O(NlogN)이다.
+# python의 str 함수는 숫자를 반복적으로 10으로 나누는 연산이
+# 발생하기 때문에 O(logN)으로 따질 수 있음).

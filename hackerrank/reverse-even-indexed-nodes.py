985c45ba4be61e6df6bfcc4389dbc6120608c2ee
Myunwoo <fddsgt123@gmail.com>
2025-12-04 01:27:42 +0900

    Create reverse-even-indexed-nodes.py

---
+++ hackerrank/reverse-even-indexed-nodes.py
@@ -0,0 +1,29 @@
+def extractAndAppendSponsoredNodes(head):
+    isEven = True
+    cur = head
+    
+    evenHead = None
+    oddHead = None
+    oddTail = None
+    
+    while cur:
+        nxt = cur.next 
+        if isEven:
+            cur.next = evenHead
+            evenHead = cur
+        else:
+            if not oddHead:
+                oddHead = cur
+                oddTail = cur
+            else:
+                oddTail.next = cur
+                oddTail = cur
+            oddTail.next = None
+        cur = nxt
+        isEven = not isEven
+    
+    if not oddHead:
+        return evenHead
+    
+    oddTail.next = evenHead
+    return oddHead

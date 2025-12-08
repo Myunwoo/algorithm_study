761ce55bab60b05aeba28ecd77c00d5a5af06f9e
Myunwoo <fddsgt123@gmail.com>
2025-12-08 09:42:26 +0900

    Update leetcode2.py

--- leetcode/leetcode2.py
+++ leetcode/leetcode2.py
@@ -5,38 +5,33 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        needAdd = False
         head = ListNode()
         cur = head
+        head1 = l1
+        head2 = l2
+        carry = 0
 
-        while True:
-            if not l1 and not l2:
-                break
-            curNode = ListNode()
+        while head1 or head2 or carry:
+            val1 = head1.val if head1 else 0
+            val2 = head2.val if head2 else 0
+            s = val1 + val2 + carry
+            curVal = s % 10
+            carry = s // 10
 
-            if l1 and l2:
-                curNode.val = l1.val + l2.val + (1 if needAdd else 0)
-                l1 = l1.next
-                l2 = l2.next
-            elif l1 and not l2:
-                curNode.val = l1.val + (1 if needAdd else 0)
-                l1 = l1.next
-            elif not l1 and l2:
-                curNode.val = l2.val + (1 if needAdd else 0)
-                l2 = l2.next
-            
-            if curNode.val >= 10:
-                curNode.val %= 10
-                needAdd = True
-            else:
-                needAdd = False
-            
-            cur.next = curNode
+            cur.next = ListNode(curVal)
             cur = cur.next
-        
-        if needAdd:
-            curNode = ListNode()
-            curNode.val = 1
-            cur.next = curNode
-        
+
+            if head1:
+                head1 = head1.next
+            if head2:
+                head2 = head2.next
+
         return head.next
+    
+# 이 문제의 시간 복잡도는 l1의 길이를 n, l2의 길이를 m이라고 했을떄,
+# O(max(n, m))으로 표현 가능
+
+# l1과 l2의 참조를 헤치지 않는 선에서 head1, head2를 생성해서 문제를 풀었음
+# 그러나, 추가 메모리를 쓰지 않는 방향으로 문제를 구현하라고 요청 받으면,
+# l1,l2를 직접 사용하고, l1, l2중 한쪽을 결과 리스트로 사용하며,
+# head에 더미 노드를 생성하지 않고 첫 노드의 존재 여부를 분기처리로 처리

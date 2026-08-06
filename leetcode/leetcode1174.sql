8fd32d0ffd0c5893fec84ef7ccda1894f9c7ae08
Munwoo <fddsgt123@gmail.com>
2026-08-07 08:43:37 +0900

    .

--- leetcode/leetcode1174.sql
+++ leetcode/leetcode1174.sql
@@ -1,20 +1,20 @@
 SELECT
-    ROUND(
-        SUM(
-            CASE
-                WHEN a1.order_date = a1.customer_pref_delivery_date THEN 1
-                ELSE 0
-            END
-        ) / COUNT(*) * 100,
-        2
-    ) AS immediate_percentage
-FROM Delivery a1
+    ROUND(SUM(
+        CASE
+            WHEN a1.order_date = a1.customer_pref_delivery_date THEN 1
+            ELSE 0
+        END) / COUNT(a1.customer_id) * 100, 2) AS immediate_percentage
+FROM
+    Delivery a1
 JOIN (
     SELECT
-        customer_id,
-        MIN(order_date) AS first_order_date
-    FROM Delivery
-    GROUP BY customer_id
+        b1.customer_id
+        , MIN(b1.order_date) AS order_date
+    FROM
+        Delivery b1
+    GROUP BY
+        b1.customer_id
 ) a2
-ON a1.customer_id = a2.customer_id
-AND a1.order_date = a2.first_order_date;
+ON
+    a1.customer_id = a2.customer_id
+    AND a1.order_date = a2.order_date
--- leetcode/leetcode550.sql
+++ leetcode/leetcode550.sql
@@ -14,3 +14,24 @@ LEFT JOIN (
 ON
     a1.event_date = a2.next_day
     AND a1.player_id = a2.player_id
+
+
+SELECT
+    ROUND(SUM(CASE
+        WHEN a2.player_id IS NOT NULL THEN 1
+        ELSE 0
+    END) / COUNT(a1.player_id), 2) AS fraction
+FROM (
+    SELECT
+        b1.player_id
+        , MIN(event_date) AS first_date
+    FROM
+        Activity b1
+    GROUP BY
+        b1.player_id
+) a1
+LEFT JOIN
+    Activity a2
+ON
+    a1.player_id = a2.player_id
+    AND ADDDATE(a1.first_date, INTERVAL 1 DAY) = a2.event_date

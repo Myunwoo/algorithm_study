c1e43affb2a078718d46db0044e7a30f46da022b
Munwoo <fddsgt123@gmail.com>
2026-08-10 16:54:27 +0900

    .

--- leetcode/leetcode1731.sql
+++ leetcode/leetcode1731.sql
@@ -1,20 +1,12 @@
 SELECT
-    a1.employee_id
-    , a1.name
-    , COUNT(a2.employee_id) AS reports_count
+    a1.employee_id AS employee_id
+    , a1.name AS name
+    , COUNT(a2.reports_to) AS reports_count
     , ROUND(AVG(a2.age)) AS average_age
 FROM
     Employees a1
-JOIN (
-    SELECT
-        employee_id
-        , reports_to
-        , age
-    FROM
-        Employees
-    WHERE
-        reports_to IS NOT NULL
-) a2
+JOIN
+    Employees a2
 ON
     a1.employee_id = a2.reports_to
 GROUP BY
--- leetcode/leetcode1789.sql
+++ leetcode/leetcode1789.sql
@@ -15,3 +15,25 @@ WHERE
         HAVING
             COUNT(department_id) = 1
     )
+
+
+SELECT
+    a1.employee_id
+    , a1.department_id
+FROM
+    Employee a1
+LEFT JOIN (
+    SELECT
+        b1.employee_id
+    FROM
+        Employee b1
+    GROUP BY
+        b1.employee_id
+    HAVING
+        COUNT(department_id) = 1
+) a2
+ON
+    a1.employee_id = a2.employee_id
+WHERE 1=1
+    AND a2.employee_id IS NOT NULL
+    OR a1.primary_flag = 'Y'
